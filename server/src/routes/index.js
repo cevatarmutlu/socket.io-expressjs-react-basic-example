b9281abda2c91a38a01d559934eb8170e03c5e40
cevatarmutlu <cevatarmutlu@gmail.com>
2020-11-07 12:35:48 +0300

    routes/index.js changed

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,9 +1,43 @@
 const express = require("express");
 const router = express.Router();
 const connectDB = require("../connection");
+const Product = require("../models/product");
 
 connectDB().then(() => {
     console.log("MongoDB connection is established");
+});
+
+router.get("/products", async (req, res) => {
+    const products = await Product.find();
+    res.json(products);
+});
+
+/*
+    {
+        name: {type: String},
+        img: {type: String},
+        price: {type: Number},
+        price_history: {type: Date},
+        category: {type: String}
+    }
+*/
+
+router.get("/product-add", async (req, res) => {
+    const a = new Date();
+    const product = new Product(
+        {
+            name: "sen ağlama bir damla göz yaşın yeter",
+            img: "sen üzülmü gülüm gamzende güllerin biter",
+            price: 15.65,
+            price_history: a,
+            category: "badem - sen ağlama",
+        }
+    );
+    await product
+            .save()
+            .then(() => console.log("Product Created"));
+
+            res.send("Product Created \n");
 });
 
 module.exports = router;
