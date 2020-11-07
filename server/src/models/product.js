7ae67587bc04fc252288488c74572c2efd738337
cevatarmutlu <cevatarmutlu@gmail.com>
2020-11-07 12:25:02 +0300

    added product model

---
+++ server/src/models/product.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+const productSchema = new mongoose.Schema(
+    {
+        name: {type: String},
+        img: {type: String},
+        price: {type: Number},
+        price_history: {type: Date},
+        category: {type: String}
+    }
+);
+
+const Product = mongoose.model("Product", productSchema);
+
+module.exports = Product;
