b3e5f944d6ed759c3f991f4f243ce5dd8e3bfd5d
cevatarmutlu <cevatarmutlu@gmail.com>
2020-11-07 12:40:22 +0300

    axios requests are working properly

--- server/server.js
+++ server/server.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const http = require("http");
 const socketIo = require("socket.io");
+const cors = require("cors");
 
 const port = process.env.PORT || 4002;
 const index = require("./src/routes/index");
 
 const app = express();
+app.use(cors());
 app.use(index);
 
 const server = http.createServer(app);
